af9d8ce43294913f7aa5695747875adaa8956781
rangrang-53 <jr0503@naver.com>
2025-04-03 22:25:47 +0900

    order-service post

---
+++ order-service/src/test/java/com/example/orderservice/OrderControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.orderservice;
+
+import com.example.orderservice.menu.MenuClientAdapter;
+import com.example.orderservice.order.controller.OrderController;
+import com.example.orderservice.order.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(OrderController.class)
+public class OrderControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private MenuClientAdapter menuClientAdapter;
+
+    @Test
+    void getOrders_returnsOk() {
+        // given
+        when(orderService.findAllOrders()).thenReturn(Flux.empty());
+
+        // when + then
+        webTestClient.get()
+                .uri("/orders")
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
