6da758dcee2e7685d8c0c6168802b71e8ec6e46a
rangrang-53 <jr0503@naver.com>
2025-06-03 14:44:40 +0900

    store order update

--- src/main/java/com/example/orderservice/order/domain/StoreOrderResponseDTO.java
+++ src/main/java/com/example/orderservice/order/domain/StoreOrderResponseDTO.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class StoreOrderResponseDTO {
     private String merchantUid;
     private Long   userUid;
+    private Long   socialUid;
     private LocalDateTime createdDate;
     private LocalDateTime reservationDate;
     private String status;
